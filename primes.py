9059bd40458df094a633a6d58e39fe53215040d0
h363 <41hmorris@gmail.com>
2023-09-26 01:00:38 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if number_of_primes < 1:
+        raise ValueError("Number of primes must be greater than 0.")
     list = []
+
+    i = 2
+    while len(list) < number_of_primes:
+        is_prime = True
+
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+        if is_prime:
+            list.append(i)
+        i += 1
+
     return list
+
+print(primes(10))
